8e416742ee73ed96c43306401ba1476b5df9cbee
ThemauJR <themau278estudio@gmail.com>
2024-05-30 11:05:05 -0300

    Add files via upload

---
+++ 06-clase.js
@@ -0,0 +1,21 @@
+function simpleE1() {
+    tanteadorE1.innerHTML = Number(tanteadorE1.innerHTML) + 1
+
+}
+
+function dobleE1() {
+    tanteadorE1.innerHTML = Number(tanteadorE1.innerHTML) + 2
+
+}
+function tripleE1() {
+    tanteadorE1.innerHTML = Number(tanteadorE1.innerHTML) + 3
+
+}
+function retrocesoE1() {
+    if (number (tanteadorE1.innerHTML < 0)) {Number (tanteadorE1.innerHTML) - 1}
+
+
+
+}
+
+let tanteadorE1 = document.getElementById("tanteadorE1")
